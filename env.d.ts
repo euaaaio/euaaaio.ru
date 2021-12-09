182ab7f8aff8a7c6245b53dd38a0bb15d835af47
Eduard Aksamitov <e@edaks.co>
2021-12-10 01:22:10 +0300

    chore: cleanup types

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,3 @@
-/// <reference types="vite/client" />
-
 declare module '*.vue' {
 	import { DefineComponent } from 'vue'
 	// eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
