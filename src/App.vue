3b3480cebfb7f85d0f92e50bc472e4840fd9f987
Eduard Aksamitov <e@edaks.co>
2022-03-08 22:51:30 +0300

    refactor: better routing

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template lang="pug">
-component(:is="view")
+main.g
+	the-header
+	component(:is="view")
+the-footer
 </template>
 
 <script lang="ts">
 import { computed, defineComponent } from 'vue'
 import { useStore } from '@nanostores/vue'
 
-import IndexView from './views/IndexView.vue'
+import ConsultancyView from './views/ConsultancyView.vue'
 import { router } from './router'
+import TheHeader from './components/TheHeader.vue'
+import TheFooter from './components/TheFooter.vue'
+import IndexView from './views/IndexView.vue'
 
 export default defineComponent({
 	components: {
-		IndexView
+		TheHeader,
+		TheFooter,
+		IndexView,
+		ConsultancyView
 	},
 	setup () {
 		let page = useStore(router)
