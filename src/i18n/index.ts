5254d71ec3bc9b2764d4d743e50af6e65c078382
Eduard Aksamitov <e@edaks.co>
2022-03-11 02:07:42 +0300

    chore: cleanup

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,6 +1,7 @@
 import { app, footer, index } from '../locales/en.json'
-import { i18n } from '../stores/i18n'
+import { i18n } from './store'
 
+export { i18n } from './store'
 export let appMessages = i18n('app', app)
 export let indexMessages = i18n('index', index)
 export let footerMessages = i18n('footer', footer)
