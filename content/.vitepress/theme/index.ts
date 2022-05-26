49b74d214dfa905a0a721d527727541867b8459b
Eduard Aksamitov <e@euaaaio.ru>
2022-05-26 19:39:13 +0300

    feat: detect browser language

--- content/.vitepress/theme/index.ts
+++ content/.vitepress/theme/index.ts
@@ -5,19 +5,22 @@ import Layout from './App.vue'
 import EHeader from './components/EHeader.vue'
 import EFooter from './components/EFooter.vue'
 import EContent from './components/EContent.vue'
-import ELangSwitcher from './components/ELangSwitcher.vue'
-import ELinkExternal from './components/ELinkExternal.vue'
 import ELinkPreview from './components/ELinkPreview.vue'
+import ELinkExternal from './components/ELinkExternal.vue'
+import ELangSwitcher from './components/ELangSwitcher.vue'
+import { useBrowserLanguage } from './plugins/use-browser-language'
 
 const theme: Theme = {
 	Layout,
-	enhanceApp ({ app }) {
+	enhanceApp ({ app, router, siteData }) {
 		app.component('EHeader', EHeader)
 		app.component('EFooter', EFooter)
 		app.component('EContent', EContent)
 		app.component('ELangSwitcher', ELangSwitcher)
 		app.component('ELinkExternal', ELinkExternal)
 		app.component('ELinkPreview', ELinkPreview)
+
+		useBrowserLanguage(siteData, router)
 	}
 }
 
---
+++ content/.vitepress/theme/plugins/use-browser-language.ts
@@ -0,0 +1,43 @@
+import { nextTick, watch } from 'vue'
+import type { Router, SiteData } from 'vitepress'
+import type { Ref } from 'vue'
+
+export function useBrowserLanguage (data: Ref<SiteData>, router: Router): void {
+	if (typeof navigator !== 'undefined') {
+		let langs = data.value.langs
+		let destroy = watch(
+			() => router.route.component,
+			() => {
+				if (
+					router.route.component !== null &&
+					Object.keys(langs).includes(router.route.path)
+				) {
+					// eslint-disable-next-line unicorn/no-array-reduce
+					let appLanguages = Object.keys(langs).reduce<Record<string, string>>(
+						(prev, path) => {
+							prev[langs[path].lang] = path
+							return prev
+						},
+						{}
+					)
+
+					let userLanguages = navigator.languages
+					if (typeof navigator.languages === 'undefined') {
+						userLanguages = [navigator.language]
+					}
+
+					for (let userLanguage of userLanguages) {
+						if (userLanguage in appLanguages) {
+							router.go(appLanguages[userLanguage])
+							destroy()
+							return
+						}
+					}
+
+					destroy()
+				}
+			},
+			{ immediate: true }
+		)
+	}
+}
