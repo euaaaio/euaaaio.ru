d45084b0a715d9f30e4d5403eb6786ac572076ce
Eduard Aksamitov <e@euaaaio.ru>
2025-06-03 02:58:52 +0300

    workflow: drop deprecated `eslint-define-config`

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,3 @@
 import eslintConfigPostanu from '@postanu/eslint-config/astro'
-import { defineFlatConfig } from 'eslint-define-config'
 
-export default defineFlatConfig([...eslintConfigPostanu])
+export default [...eslintConfigPostanu]
